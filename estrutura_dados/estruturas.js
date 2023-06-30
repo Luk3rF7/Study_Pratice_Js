c1e6530a80eb7a68ab2461f66228ff81dcf68c33
luk3rF7 <lkzeron@gmail.com>
2023-06-29 23:55:45 -0300

    att

--- estrutura_dados/estruturas.js
+++ estrutura_dados/estruturas.js
@@ -1,2 +1,30 @@
 // lista
 const convidados = ["javascript"];
+
+//utilizo push para acrescentar
+
+convidados.push("Python");
+
+console.log(convidados);
+const curso = ["Java", "C", "C#"];
+curso.push(...convidados);
+
+console.log(curso);
+curso.splice(3, 1);
+console.log(curso);
+
+//pilha
+
+curso.push("javascript");
+console.log(curso);
+curso.push("PHP");
+curso.push("");
+console.log(curso);
+
+curso[6] = "Html";
+console.log(curso);
+
+if (curso.length > 0) {
+  curso.shift();
+}
+console.log(curso);
